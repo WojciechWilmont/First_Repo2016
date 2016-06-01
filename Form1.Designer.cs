a1210c247e1dc1d1cae13260503d93e4a6e86172
Tajgerson <tajgersonww@gmail.com>
2016-06-01 17:26:50 +0200

    Final commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
@@ -47,6 +47,11 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(332, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(288, 20);
             this.numericUpDown1.TabIndex = 1;
@@ -57,16 +62,17 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(175, 44);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Wyślij";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox3
+            // textBox2
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 295);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(608, 51);
-            this.textBox3.TabIndex = 4;
+            this.textBox2.Location = new System.Drawing.Point(12, 295);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(608, 51);
+            this.textBox2.TabIndex = 4;
             // 
             // listBox1
             // 
@@ -82,7 +88,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 439);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.textBox1);
@@ -99,7 +105,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ListBox listBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net.Sockets;
 namespace UDP_klient
 {
     public partial class Form1 : Form
@@ -20,6 +20,27 @@ namespace UDP_klient
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string host = textBox1.Text;
+            int port = System.Convert.ToInt16(numericUpDown1.Value);
+
+            try
+            {
+                UdpClient klient = new UdpClient(host, port);
+                Byte[] dane = Encoding.ASCII.GetBytes(textBox2.Text);
+                klient.Send(dane, dane.Length);
+                listBox1.Items.Add("Wysłanie wiadomości do hosta" + host + ": " + port);
+                klient.Close();
+            }
+                catch(Exception ex){
+                    listBox1.Items.Add("Błąd: Nie udało się wysłać wiadomości");
+                    MessageBox.Show(ex.ToString(), "Błąd");
+                }
+
+            
         }
     }
 }
